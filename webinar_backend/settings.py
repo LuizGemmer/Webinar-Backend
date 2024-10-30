a3c9fbe4f852543d4651478a45130a41e178e8ab
LuizGemmer <luiz.o.gemmer@gmail.com>
2024-10-29 21:04:19 -0300

    tentando resolver conexão com o bd na vercel

--- webinar_backend/settings.py
+++ webinar_backend/settings.py
@@ -144,7 +144,7 @@ WSGI_APPLICATION = 'webinar_backend.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
