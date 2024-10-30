6b9931deedf47ecaeb33670f017b838c33c994a8
LuizGemmer <luiz.o.gemmer@gmail.com>
2024-10-29 21:14:22 -0300

    tentando resolver conexão com o bd na vercel

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput    
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput    
