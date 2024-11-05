9ef0b6cc8c0e007478b72baa383393fde578a566
LuizGemmer <luiz.o.gemmer@gmail.com>
2024-11-05 20:59:30 -0300

    adicionado criação de super usuário no build

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ python3.12 -m pip install -r requirements.txt
 python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput    
 
-python3.12 manage.py collectstatic --noinput --clear    
+python3.12 manage.py collectstatic --noinput --clear 
+
+python3.12 createsuperuser --no-input
