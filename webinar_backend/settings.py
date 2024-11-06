4a05d9bcbf26c5cdb8c2e6c364f1cc7c687539f8
LuizGemmer <luiz.o.gemmer@gmail.com>
2024-11-06 20:42:43 -0300

    ajustado settings.py

--- webinar_backend/settings.py
+++ webinar_backend/settings.py
@@ -195,7 +195,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 MEDIA_URL = os.environ.get('MEDIA_URL')
-MEDIA_ROOT = MEDIA_URL / "media/"
+MEDIA_ROOT = MEDIA_URL + "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
