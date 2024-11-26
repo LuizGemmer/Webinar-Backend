7054a3d48644ac03ae8464cf33021acbc9f8c399
LuizGemmer <luiz.o.gemmer@gmail.com>
2024-11-25 21:27:24 -0300

    amazon s3 config

--- build.sh
+++ build.sh
@@ -5,5 +5,3 @@ python3.12 -m pip install -r requirements.txt
 python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput    
 
-python3.12 manage.py collectstatic --noinput --clear 
-
--- webinar_backend/settings.py
+++ webinar_backend/settings.py
@@ -271,7 +271,7 @@ QUIZ_RELATED_CLASS_MODEL = "permissions.Function"
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_STORAGE_BUCKET_NAME = 'ruisu-webinar-bucket'
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
